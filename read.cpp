726d2f162df7b6a074bc98d88acf3273a025c25e
Gabriel Bondi <enigmae7889@gmail.com>
2023-11-09 17:52:42 +0100

    10/12, fit function does not work properly

--- read.cpp
+++ read.cpp
@@ -6,25 +6,43 @@
 #include <iostream>
 
 #include "TCanvas.h"
+#include "TF1.h"
 #include "TFile.h"
 #include "TH1.h"
 #include "TH2.h"
+#include "TMath.h"
 #include "TROOT.h"
 #include "TRandom.h"
 
 void read() {
   TFile *file = new TFile("HistogramsFile.root", "READ");
 
-  int NHistograms = 1;
+  int NHistograms = 12;
   TH1D *h[NHistograms];
+
   int j{0};
+
   h[j] = (TH1D *)file->Get("PTH");
+
   for (int k{0}; k < 9; ++k) {
-    std::cout << h[j]->GetBinContent(k) / 1.E7 << std::endl;
-    std::cout << h[j]->GetBinError(k) / 1.E7 << std::endl << std::endl;
+    std::cout << h[j]->GetBinContent(k) / 1.E7 << " +/- " << h[j]->GetBinError(k) / 1.E7 << std::endl;
   }
   ++j;
+
   h[j] = (TH1D *)file->Get("IH");
+  /*
+  TF1 *f1 = new TF1("f1", myFunction, 0, 5, 1);
+  f1->SetParameter(0, 1);
+
+  h[j]->Fit("f1");
+
+  TF1 *ff1 = h[j]->GetFunction("f1");
+
+  std::cout << ff1->GetChisquare() / ff1->GetNDF() << std::endl;
+  std::cout << ff1->GetParameter(1) << " +/- " << ff1->GetParError(1) << std::endl;
+  std::cout << ff1->GetProb() << std::endl << std::endl;
+  */
+
   ++j;
   h[j] = (TH1D *)file->Get("TIH");
   ++j;
@@ -48,19 +66,28 @@ void read() {
 
   TH2D *h2 = (TH2D *)file->Get("AH");
 
-  TCanvas *c1 = new TCanvas("c1", "c1", 10, 20, 500, 600);
-  // c1->Divide(3, 4);
+  TCanvas *c1 = new TCanvas("c1", "c1", 10, 20, 1000, 600);
+  c1->Divide(3, 2);
 
-  for (int i{0}; i < NHistograms; ++i) {
+  for (int i{0}; i < NHistograms / 2; ++i) {
     c1->cd(i + 1);
     h[i]->DrawCopy();
   }
 
-  TCanvas *c2 = new TCanvas("c2", "c2", 10, 20, 500, 600);
+  TCanvas *c2 = new TCanvas("c2", "c2", 10, 20, 1000, 600);
   h2->DrawCopy();
+
+  TCanvas *c3 = new TCanvas("c3", "c3", 10, 20, 1000, 600);
+  c3->Divide(3, 2);
+
+  for (int i{5}; i < NHistograms; ++i) {
+    c3->cd(i - 5 + 1);
+    h[i]->DrawCopy();
+  }
 
   file->Close();
 
   c1->Print("myCanvas1.gif");
   c2->Print("myCanvas2.gif");
+  c3->Print("myCanvas3.gif");
 }
